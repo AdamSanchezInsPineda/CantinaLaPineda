4f30aba19325efe97ff21c7d429c245c39e5cece
MiguelAngelMateos <mateosmiguelangel3@gmail.com>
2025-01-29 12:16:37 +0100

    vista temporal de listados en el panel de admin

--- resources/views/components/admin-aside.blade.php
+++ resources/views/components/admin-aside.blade.php
@@ -1,8 +1,8 @@
-<aside class="fixed top-0 left-0 h-full w-[300px] bg-gray-100 p-4 shadow-md border-r-2">
+<aside class="fixed top-0 left-0 h-full w-[300px] bg-white p-4 shadow-md border-r-2">
     <ul class="flex flex-col gap-12">
         <div class="mb-40"></div> <!-- esto no lo ha visto nadie... -->
         <a href="">
-            <li class="border-b-2 text-xl flex gap-4">
+            <li class="border-b-2 text-xl flex gap-4 items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                 </svg>              
@@ -10,7 +10,7 @@
             </li>
         </a>
         <a href="">
-            <li class="border-b-2 text-xl flex gap-4">
+            <li class="border-b-2 text-xl flex gap-4 items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="size-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
                 </svg>                         
@@ -18,7 +18,7 @@
             </li>
         </a>
         <a href="">
-            <li class="border-b-2 text-xl flex gap-4">
+            <li class="border-b-2 text-xl flex gap-4 items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
@@ -27,7 +27,7 @@
             </li>
         </a>
         <a href="">
-            <li class="border-b-2 text-xl flex gap-4">
+            <li class="border-b-2 text-xl flex gap-4 items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                 </svg>                           

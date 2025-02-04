6d868c22e0d49b947000da088b9f1d3edd83aa04
MiguelAngelMateos <mateosmiguelangel3@gmail.com>
2025-02-04 10:07:38 +0100

    controladores y rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +19,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resources([
+    'category' => CategoryController::class,
+    'product' => ProductController::class,
+    'order' => OrderController::class
+]);
 
 require __DIR__.'/auth.php';

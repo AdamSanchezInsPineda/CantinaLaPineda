7b27b44c80c3c945c33cfe42544af9b3562dbfda
MiguelAngelMateos <mateosmiguelangel3@gmail.com>
2025-02-21 11:35:59 +0100

    pequeño arreglo

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -41,7 +41,7 @@
                 <p class="text-xl">{{ $product->price }} €</p>
                 <p class="text-l">Quedan {{ $product->stock }}!</p>
                 <p class="break-words max-w-[500px] whitespace-pre-wrap text-sm text-gray-700">{{ $product->description }}</p>
-                <a href="" class="mt-10 bg-gray-800 text-white py-2 px-4 rounded-full h-[40px] w-[200px] hover:bg-gray-700 flex items-center justify-center gap-3">
+                <a href="" class="mt-10 mb-20 bg-gray-800 text-white py-2 px-4 rounded-full h-[40px] w-[200px] hover:bg-gray-700 flex items-center justify-center gap-3">
                     <span>Añadir al carrito</span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />

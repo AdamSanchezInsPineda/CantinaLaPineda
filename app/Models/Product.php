342a8d66a936c4d8ac34290575e286ca0989f272
AdamSanchezInsPineda <adam.sanchez@inslapineda.cat>
2025-02-21 12:06:36 +0100

    Stock eliminado

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,7 +14,7 @@ class Product extends Model
     protected $casts = [
         'images' => 'array',
     ];    
-    protected $fillable = ['category_id', 'code', 'name', 'description', 'price', 'stock', 'featured', 'images'];
+    protected $fillable = ['category_id', 'code', 'name', 'description', 'price', 'featured', 'images'];
     //
     public function category(): BelongsTo {
         return $this->belongsTo(Category::class);
--- database/migrations/2025_01_24_110215_create_products_table.php
+++ database/migrations/2025_01_24_110215_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('description')->nullable();
             $table->float('price');
-            $table->integer('stock');
             $table->boolean('featured')->default(false);
             $table->timestamps();
             $table->json('images')->nullable();

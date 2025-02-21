d053843d3ef429df243af5af0de49fd9b523fc1a
MiguelAngelMateos <mateosmiguelangel3@gmail.com>
2025-02-21 13:24:06 +0100

    preferences en el aside del admin

--- resources/views/components/admin-aside.blade.php
+++ resources/views/components/admin-aside.blade.php
@@ -2,7 +2,7 @@
     <ul class="flex flex-col gap-12">
         <div class="mb-40"></div> <!-- esto no lo ha visto nadie... -->
         <a href="{{ route('admin.product.index') }}">
-            <li class="rounded-md p-1 border-b-2 text-xl flex gap-4 items-center {{ request()->routeIs('product.index') ? 'bg-gray-300' : 'hover:bg-gray-100' }}">
+            <li class="rounded-md p-1 border-b-2 text-xl flex gap-4 items-center {{ request()->routeIs('admin.product.index') ? 'bg-gray-300' : 'hover:bg-gray-100' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                 </svg>              
@@ -10,7 +10,7 @@
             </li>
         </a>
         <a href="{{ route('admin.order.index') }}">
-            <li class="rounded-md p-1 border-b-2 text-xl flex gap-4 items-center {{ request()->routeIs('order.index') ? 'bg-gray-300' : 'hover:bg-gray-100' }}">
+            <li class="rounded-md p-1 border-b-2 text-xl flex gap-4 items-center {{ request()->routeIs('admin.order.index') ? 'bg-gray-300' : 'hover:bg-gray-100' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="size-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
                 </svg>                         
@@ -18,7 +18,7 @@
             </li>
         </a>
         <a href="{{ route('admin.category.index') }}">
-            <li class="rounded-md p-1 border-b-2 text-xl flex gap-4 items-center {{ request()->routeIs('category.index') ? 'bg-gray-300' : 'hover:bg-gray-100' }}">
+            <li class="rounded-md p-1 border-b-2 text-xl flex gap-4 items-center {{ request()->routeIs('admin.category.index') ? 'bg-gray-300' : 'hover:bg-gray-100' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
@@ -35,4 +35,13 @@
             </li>
         </a>
     </ul>
+    <a href="{{ route('admin.preference.index') }}">
+        <div class="ml-5 rounded-md p-1 border-b-2 text-xl flex gap-4 items-center {{ request()->routeIs('admin.preference.index') ? 'bg-gray-300' : 'hover:bg-gray-100' }} mt-auto fixed bottom-10 left-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>                  
+            <span>Configuraciones</span>
+        </div>
+    </a>
 </aside>

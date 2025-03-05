7062c55e42900ef30e9818b8179d5ead3bfb21fe
MiguelAngelMateos <mateosmiguelangel3@gmail.com>
2025-03-05 10:15:53 +0100

    factura temporal

--- app/Http/Controllers/Public/OrderController.php
+++ app/Http/Controllers/Public/OrderController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\PublicController;
+use App\Models\Order;
+use App\Models\OrderProduct;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class OrderController extends PublicController
 {
     //
+    public function show(string $id) 
+    {
+        $order = Order::with('products')->find($id);
+
+        $pdf = Pdf::loadView('public.orders.generatePDF', ['order' => $order]);
+        
+        return $pdf->download('factura.pdf');
+    }
 }

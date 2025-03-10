b2bebd812276ca9d00dbb70cd81b2493b09ad465
MiguelAngelMateos <mateosmiguelangel3@gmail.com>
2025-03-10 12:21:14 +0100

    añadidas migraciones, modelos y seeders para personalizar productos segun su categoria (ejemplo: añadir queso en bocadillos)

---
+++ app/Models/CategoryParameter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CategoryParameter extends Model
+{
+    //
+    use HasFactory;
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
+    public function products(): BelongsToMany {
+        return $this->belongsToMany(Product::class, 'product_parameters')->withPivot('value');
+    }
+}
---
+++ database/seeders/CategoryParameterSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CategoryParameter;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoryParameterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        CategoryParameter::factory()->create([
+            'key' => "queso",
+            'description' => "Añadir queso",
+            'category_id' => 1
+        ]);
+    }
+}

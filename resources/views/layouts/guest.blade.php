cce0a073460e9d86e2950b0059ec917284670bec
MiguelAngelMateos <mateosmiguelangel3@gmail.com>
2025-03-24 12:18:16 +0100

    accesibilidad en login y registro

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,16 +16,16 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div class="flex flex-col items-center gap-10">
+            <header class="flex flex-col items-center gap-10">
                 <a href="/">
                     <img src="{{ asset('storage/logo/logo.png') }}" alt="logo insituto" class="max-w-[300px] h-auto">
                 </a>
-                <div class="text-xl font-bold">Bienvenido/a a la cantina!</div>
-            </div>
+                <h1 class="text-xl font-bold">Bienvenido/a a la cantina!</h1>
+            </header>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <main class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
-            </div>
+            </main>
         </div>
     </body>
 </html>
